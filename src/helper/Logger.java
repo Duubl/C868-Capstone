42d1c6df7706570bca22e55080c5983c2cc0f7a7
Duubl <147439692+Duubl@users.noreply.github.com>
2024-10-20 15:35:11 -0700

    Javadoc for logLogin method

--- src/helper/Logger.java
+++ src/helper/Logger.java
@@ -10,6 +10,13 @@ import java.time.format.DateTimeFormatter;
 public class Logger {
     public static final String log = "login_activity.txt";
 
+    /**
+     * Writes login information to a text file. Is appended the username, result, date and time to the end of the file on every attempt.
+     * @param username the username of the user who attempted to sign on.
+     * @param success the result of the sign in attempt. Either true or false.
+     * @throws IOException
+     */
+
     public static void logLogin(String username, boolean success) throws IOException {
         FileWriter fileWriter = new FileWriter(log, true);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy hh:mm:ss");

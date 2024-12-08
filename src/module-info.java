0cfb35bf8ea52c08ad4647a10f419ecb20f999c0
Duubl <147439692+Duubl@users.noreply.github.com>
2024-12-07 16:02:50 -0800

    Added requirements
    Added requirement for mysql.connector.j

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module duble.wgu.c195 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
 
     opens controller to javafx.fxml;
     opens model to javafx.fxml;
